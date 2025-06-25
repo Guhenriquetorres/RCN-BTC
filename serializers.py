5e3daafa24511621eab2848a64f00fd30f9c2547
Guhenriquetorres <106326064+Guhenriquetorres@users.noreply.github.com>
2025-06-25 17:25:50 -0300

    Delete serializers.py

--- serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import PrevisaoBTC
-
-class PrevisaoBTCSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = PrevisaoBTC
-        fields = '__all__'
