ed1a1e15368e81d6767ae24770c4b28b37ebcbcb
Guhenriquetorres <106326064+Guhenriquetorres@users.noreply.github.com>
2025-06-25 17:26:01 -0300

    Delete models.py

--- models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-class PrevisaoBTC(models.Model):
-    data = models.DateField()
-    preco_previsto = models.FloatField()
-    modelo = models.CharField(max_length=100)
-
-    def __str__(self):
-        return f"{self.data} - {self.modelo}"
