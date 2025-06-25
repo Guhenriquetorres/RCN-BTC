236ea8b5ea3b86ee8cd14ce36140c030163eb977
Guhenriquetorres <106326064+Guhenriquetorres@users.noreply.github.com>
2025-06-25 17:25:09 -0300

    Delete views.py

--- views.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import viewsets
-from .models import PrevisaoBTC
-from .serializers import PrevisaoBTCSerializer
-
-class PrevisaoBTCViewSet(viewsets.ModelViewSet):
-    queryset = PrevisaoBTC.objects.all()
-    serializer_class = PrevisaoBTCSerializer
